a75679869586482ab24fe08888198992502d971c
biasferr <bia.sferr07@gmail.com>
2026-06-10 14:53:37 -0300

    Update jogoG3.py

--- jogoG3.py
+++ jogoG3.py
@@ -5,39 +5,46 @@ init()
 
 window = display.set_mode((1056,624))
 
-tileset_parede = image.load('Modern_Interiors/Modern tiles_Free/Old/Tileset_32x32_1.png')
-tileset = image.load('Modern_Interiors/Modern tiles_Free/Old/Tileset_32x32_16.png')
+tileset_parede = image.load('moderninteriors-win/1_Interiors/32x32/Room_Bulder_subfiles_32x32/Room_Builder_Walls_32x32.png')
+tileset_bordas = image.load('moderninteriors-win/1_Interiors/32x32/Room_Bulder_subfiles_32x32/Room_Builder_borders_32x32.png')
+tileset_floor = image.load('moderninteriors-win/1_Interiors/32x32/Room_Bulder_subfiles_32x32/Room_Builder_Floors_32x32.png')
+
 tilesize = 32
 
 
 mapa = [
     ['f','f','f','f'],
-    ['cse', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2','pbs1','pbs2','pbs1','pbs2','csd'],
-    ['be',  'pi1',  'pi2',  'pi1',  'pi2',  'pi1',  'pi2',  'pi1',  'pi2', 'pi1', 'pi2',  'pi1', 'pi2','bd'],
+    ['cse', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2','pbs2','pbs2','pbs2','pbs2','csd'],
+    ['be',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi', 'pi', 'pi',  'pi', 'pi','bd'],
     ['be', 'fcse',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs', 'fbs','fbs',  'fbs', 'fbs', 'bd'],
-    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'cse', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2','pbs1','pbs2','csd' ],
-    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'be',  'pi1',  'pi2',  'pi1',  'pi2',  'pi1',  'pi2',  'pi1',  'pi2', 'pi1', 'pi2', 'bd' ],
-    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd' ],
-    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd' ],
-    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd' ],
-    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd' ],
-    ['be', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2','pbs1','pbs2','pbs1','pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2', 'pbs1', 'pbs2','bd'],
-    ['be',  'pi1',  'pi2',  'pi1',  'pi2',  'pi1',  'pi2',  'pi1',  'pi2', 'pi1', 'pi2',  'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2', 'pi1', 'pi2','bd', ]
-
+    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'cse', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2','pbs2','pbs2','csd' ],
+    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'be',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi', 'pi', 'pi', 'bd' ],
+    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'be', 'fcse', 'fbs', 'fbs', 'fbs', 'fbs', 'fbs', 'fbs', 'fbs', 'fbs', 'fbs', 'bd'],
+    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'be', 'fbe', 'floor','floor','floor','floor','floor','floor','floor','floor','floor','bd'],
+    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'be', 'fbe', 'floor','floor','floor','floor','floor','floor','floor','floor','floor','bd' ],
+    ['be', 'fbe', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'floor', 'bd', 'f', 'be', 'fbe', 'floor','floor','floor','floor','floor','floor','floor','floor','floor','bd' ],
+    ['be', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2','pbs2','pbs2','pbs2','pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2', 'pbs2','csd'],
+    ['be',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi',  'pi', 'pi', 'pi',  'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi', 'pi','bd', ],
+    ['be', 'fcse',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs', 'fbs','fbs',  'fbs', 'fbs','fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs', 'fbs','fbs',  'fbs', 'fbs','fbs',  'fbs',  'fbs',  'fbs',  'fbs',  'fbs','bd'],
+    ['be', 'fbe',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor', 'floor','floor',  'floor', 'floor','floor',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor', 'floor','floor',  'floor', 'floor','floor',  'floor',  'floor',  'floor',  'floor',  'floor','bd'],
+    ['be', 'fbe',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor', 'floor','floor',  'floor', 'floor','floor',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor',  'floor', 'floor','floor',  'floor', 'floor','floor',  'floor',  'floor',  'floor',  'floor',  'floor','bd'],
+    ['cie','bi', 'bi', 'bi', 'bi', 'bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','bi','cid']
         ]
 blocos_escola = {
-    'cse': (0,32),
-    'pbs1': (32,32),
-    'pbs2': (64,32),
-    'csd': (64,32),
-    'be': (0,64),
-    'pi1': (32,64),
-    'pi2': (64,64),
-    'bd': (64,64),
-    'fcse': (32,96),
-    'fbs': (64,96),
-    'fbe': (32,128),
-    'floor': (64,128)
+    'cse': (192,192),
+    'pbs1': (0,576),
+    'pbs2': (32,576),
+    'csd': (256,192),
+    'be': (192,224),
+    'pi': (32,608),
+    'bd': (256,224),
+    'fcse': (0,640),
+    'fbs': (32,640),
+    'fbe': (0,672),
+    'floor': (32,672),
+    'cie': (192,256),
+    'cid': (256,256),
+    'bi': (224,256)
 
 
 }
@@ -58,12 +65,15 @@ while True:
             if tile == 'f':
                 draw.rect(window,(0,0,0),(tilesize*j, tilesize*i, tilesize, tilesize))
 
-            elif 'p' in tile or 'f' in tile:
+            elif 'p' in tile:
                 if coordenada_escola:
                     window.blit(tileset_parede,(tilesize * j , tilesize*i),(coordenada_escola[0],coordenada_escola[1],tilesize,tilesize))           
-
+            elif 'f' in tile:
+                if coordenada_escola:
+                    window.blit(tileset_floor,(tilesize * j , tilesize*i),(coordenada_escola[0],coordenada_escola[1],tilesize,tilesize))           
+       
             else:
                 if coordenada_escola:
-                    window.blit(tileset,(tilesize * j , tilesize*i),(coordenada_escola[0],coordenada_escola[1],tilesize,tilesize))           
+                    window.blit(tileset_bordas,(tilesize * j , tilesize*i),(coordenada_escola[0],coordenada_escola[1],tilesize,tilesize))           
 
     display.update()
